e3a22f061e6e7f447cef1ce70c1b6d880ef6fe05
sjmoon00 <paulmoon00@naver.com>
2026-04-23 17:14:32 +0900

    init

---
+++ src/test/java/com/realteeth/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realteeth.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
